e7ea4f7a5f3df0dbc867d39f8871be5cc915330b
Tod Bachman <tod.bachman@workiva.com>
2020-05-29 16:27:26 -0600

    Add hack to get Bazz button working

--- web/react_render_fixes.dart
+++ web/react_render_fixes.dart
@@ -16,6 +16,22 @@ Function(Node, Function(Node)) addShadowRoot =
     JSFunction('hostDiv', 'render', '''
       const shadow = hostDiv.attachShadow({ mode: 'open' });
       this.mountPoint = document.createElement('div');
+      function changeOwnerDocumentToShadowRoot(element, appContainer) {
+          Object.defineProperty(element, 'ownerDocument', {value: appContainer});
+      }
+      function augmentAppendChildWithOwnerDocument(elem, appContainer) {
+        const origAppChild = elem.appendChild;
+        const propDesc = Object.getOwnPropertyDescriptor(elem, 'appendChild');
+        if (!propDesc || propDesc.writable) {
+            Object.defineProperty(elem, 'appendChild', {
+                value: function(child) {
+                    changeOwnerDocumentToShadowRoot(child, appContainer);
+                    origAppChild?.call(elem, child);
+                }
+            });
+        }
+      }
+      augmentAppendChildWithOwnerDocument(this.mountPoint, shadow);
       shadow.appendChild(this.mountPoint);
       Object.defineProperty(this.mountPoint, 'ownerDocument', { value: shadow });
       shadow.createElement =  function(one, two) { return document.createElement(...arguments); };
